1574d6920ed909c6a09078b04414d4b44e095859
alexbarnett12 <alexander.barnett@vanderbilt.edu>
2019-08-02 16:25:56 -0500

    Replaced flags with config for ease of modification

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ import os
 import random
 import time
 import sys
+import json
 
 from absl import app
 from absl import flags
@@ -45,9 +46,7 @@ from differential_base_state import DifferentialBaseState
 import packages.ml
 
 gfile = tf.gfile
-
-# M
-MAX_TO_KEEP = 1000000  # Maximum number of checkpoints to keep.
+CONFIG_PATH = "/mnt/isaac/apps/carter_sim_struct2depth/configs/train_parameters.json"
 
 # Map strings
 WAREHOUSE = 'carter_warehouse_p'
@@ -63,92 +62,91 @@ flags.DEFINE_string('map_config_filename', "apps/assets/maps/" + WAREHOUSE + ".c
 flags.DEFINE_string('map_graph_filename', "apps/assets/maps/" + WAREHOUSE + ".graph.json",
                     "Graph file for Isaac Sim map")
 
-# Tensorflow flags
-flags.DEFINE_string('data_dir', '/mnt',
-                    'Preprocessed data.')
-flags.DEFINE_string('file_extension', 'png', 'Image data file extension.')
-flags.DEFINE_float('learning_rate', 0.0002, 'Adam learning rate.')
-flags.DEFINE_float('beta1', 0.9, 'Adam momentum.')
-flags.DEFINE_float('reconstr_weight', 0.85, 'Frame reconstruction loss weight.')
-flags.DEFINE_float('ssim_weight', 0.15, 'SSIM loss weight.')
-flags.DEFINE_float('smooth_weight', 0.04, 'Smoothness loss weight.')
-flags.DEFINE_float('icp_weight', 0.0, 'ICP loss weight.')
-flags.DEFINE_float('size_constraint_weight', 0.0005, 'Weight of the object '
-                                                     'size constraint loss. Use only when motion handling is '
-                                                     'enabled.')
-flags.DEFINE_integer('batch_size', 4, 'The size of a sample batch')
-flags.DEFINE_integer('img_height', 128, 'Input frame height.')
-flags.DEFINE_integer('img_width', 416, 'Input frame width.')
-flags.DEFINE_integer('seq_length', 3, 'Number of frames in sequence.')
-flags.DEFINE_enum('architecture', nets.RESNET, nets.ARCHITECTURES,
-                  'Defines the architecture to use for the depth prediction '
-                  'network. Defaults to ResNet-based encoder and accompanying '
-                  'decoder.')
-flags.DEFINE_boolean('imagenet_norm', True, 'Whether to normalize the input '
-                                            'images channel-wise so that they match the distribution '
-                                            'most ImageNet-models were trained on.')
-flags.DEFINE_float('weight_reg', 0.05, 'The amount of weight regularization to '
-                                       'apply. This has no effect on the ResNet-based encoder '
-                                       'architecture.')
-flags.DEFINE_boolean('exhaustive_mode', False, 'Whether to exhaustively warp '
-                                               'from any frame to any other instead of just considering '
-                                               'adjacent frames. Where necessary, multiple egomotion '
-                                               'estimates will be applied. Does not have an effect if '
-                                               'compute_minimum_loss is enabled.')
-flags.DEFINE_boolean('random_scale_crop', False, 'Whether to apply random '
-                                                 'image scaling and center cropping during training.')
-flags.DEFINE_enum('flipping_mode', reader.FLIP_RANDOM,
-                  [reader.FLIP_RANDOM, reader.FLIP_ALWAYS, reader.FLIP_NONE],
-                  'Determines the image flipping mode: if random, performs '
-                  'on-the-fly augmentation. Otherwise, flips the input images '
-                  'always or never, respectively.')
-flags.DEFINE_string('pretrained_ckpt',
-                    '/mnt/isaac/apps/carter_sim_struct2depth/struct2depth/pretrained_ckpt/model-199160.ckpt',
-                    'Path to checkpoint with '
-                    'pretrained weights.  Do not include .data* extension.')
-flags.DEFINE_string('imagenet_ckpt',
-                    '/mnt/isaac/apps/carter_sim_struct2depth/struct2depth/resnet_pretrained/model.ckpt',
-                    'Initialize the weights according '
-                    'to an ImageNet-pretrained checkpoint. Requires '
-                    'architecture to be ResNet-18.')
-flags.DEFINE_string('checkpoint_dir',
-                    '/mnt/ckpts/sim/warehouse/40_delay_pretrained_7_31_2',
-                    'Directory to save model '
-                    'checkpoints.')
-flags.DEFINE_integer('train_steps', 10000000, 'Number of training steps.')
-flags.DEFINE_integer('summary_freq', 4, 'Save summaries every N steps.')
-flags.DEFINE_bool('depth_upsampling', True, 'Whether to apply depth '
-                                            'upsampling of lower-scale representations before warping to '
-                                            'compute reconstruction loss on full-resolution image.')
-flags.DEFINE_bool('depth_normalization', True, 'Whether to apply depth '
-                                               'normalization, that is, normalizing inverse depth '
-                                               'prediction maps by their mean to avoid degeneration towards '
-                                               'small values.')
-flags.DEFINE_bool('compute_minimum_loss', True, 'Whether to take the '
-                                                'element-wise minimum of the reconstruction/SSIM error in '
-                                                'order to avoid overly penalizing dis-occlusion effects.')
-flags.DEFINE_bool('use_skip', True, 'Whether to use skip connections in the '
-                                    'encoder-decoder architecture.')
-flags.DEFINE_bool('equal_weighting', False, 'Whether to use equal weighting '
-                                            'of the smoothing loss term, regardless of resolution.')
-flags.DEFINE_bool('joint_encoder', False, 'Whether to share parameters '
-                                          'between the depth and egomotion networks by using a joint '
-                                          'encoder architecture. The egomotion network is then '
-                                          'operating only on the hidden representation provided by the '
-                                          'joint encoder.')
-
-''' MODIFIED THIS TO BE FALSE FOR THE TIME BEING'''
-flags.DEFINE_bool('handle_motion', False, 'Whether to try to handle motion by '
-                                          'using the provided segmentation masks.')
-flags.DEFINE_string('master', 'local', 'Location of the session.')
-
 FLAGS = flags.FLAGS
 
-# flags.mark_flag_as_required('data_dir')
-# flags.mark_flag_as_required('checkpoint_dir')
+def load_parameters():
+    with open(CONFIG_PATH) as f:
+        config = json.load(f)
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # GPU to run
+    return config["data_dir"], \
+           config["using_saved_images"], \
+           config["pretrained_ckpt"], \
+           config["imagenet_ckpt"], \
+           config["checkpoint_dir"], \
+           config["isaac_app_filename"], \
+           config["cuda_device"], \
+           config["file_extension"], \
+           config["batch_size"], \
+           config["save_ckpt_every"], \
+           config["learning_rate"], \
+           config["beta1"], \
+           config["reconstr_weight"], \
+           config["ssim_weight"], \
+           config["smooth_weight"], \
+           config["icp_weight"], \
+           config["size_constraint_weight"], \
+           config["img_height"], \
+           config["img_width"], \
+           config["seq_length"], \
+           config["architecture"], \
+           config["imagenet_norm"], \
+           config["weight_reg"], \
+           config["exhaustive_mode"], \
+           config["random_scale_crop"], \
+           config["flipping_mode"], \
+           config["train_steps"], \
+           config["summary_freq"], \
+           config["depth_upsampling"], \
+           config["depth_normalization"], \
+           config["compute_minimum_loss"], \
+           config["use_skip"], \
+           config["equal_weighting"], \
+           config["joint_encoder"], \
+           config["handle_motion"], \
+           config["master"], \
+           config["shuffle"], \
+           config["max_ckpts_to_keep"]
+
+
+def verify_parameters(handle_motion, joint_encoder, seq_length, compute_minimum_loss, img_height, img_width,
+                      imagenet_ckpt,imagenet_norm, architecture, exhaustive_mode, icp_weight, checkpoint_dir):
+    if handle_motion and joint_encoder:
+        raise ValueError('Using a joint encoder is currently not supported when '
+                         'modeling object motion.')
+    if handle_motion and seq_length != 3:
+        raise ValueError('The current motion model implementation only supports '
+                         'using a sequence length of three.')
+    if handle_motion and not compute_minimum_loss:
+        raise ValueError('Computing the minimum photometric loss is required when '
+                         'enabling object motion handling.')
+    # if FLAGS.size_constraint_weight > 0 and not FLAGS.handle_motion:
+    #   raise ValueError('To enforce object size constraints, enable motion '
+    #                    'handling.')
+    if imagenet_ckpt and not imagenet_norm:
+        logging.warn('When initializing with an ImageNet-pretrained model, it is '
+                     'recommended to normalize the image inputs accordingly using '
+                     'imagenet_norm.')
+    if compute_minimum_loss and seq_length % 2 != 1:
+        raise ValueError('Compute minimum loss requires using an odd number of '
+                         'images in a sequence.')
+    if architecture != nets.RESNET and imagenet_ckpt:
+        raise ValueError('Can only load weights from pre-trained ImageNet model '
+                         'when using ResNet-architecture.')
+    if compute_minimum_loss and exhaustive_mode:
+        raise ValueError('Exhaustive mode has no effect when compute_minimum_loss '
+                         'is enabled.')
+    if img_width % (2 ** 5) != 0 or img_height % (2 ** 5) != 0:
+        logging.warn('Image size is not divisible by 2^5. For the architecture '
+                     'employed, this could cause artefacts caused by resizing in '
+                     'lower dimensions.')
+    if icp_weight > 0.0:
+        raise ValueError('ICP is currently not supported.')
+
+    if checkpoint_dir is None:
+        raise ValueError('Must specify a checkpoint directory')
+
+    if not gfile.Exists(checkpoint_dir):
+        gfile.MakeDirs(checkpoint_dir)
 
 
 def main(_):
@@ -158,41 +156,49 @@ def main(_):
     np.random.seed(seed)
     random.seed(seed)
 
-    if FLAGS.handle_motion and FLAGS.joint_encoder:
-        raise ValueError('Using a joint encoder is currently not supported when '
-                         'modeling object motion.')
-    if FLAGS.handle_motion and FLAGS.seq_length != 3:
-        raise ValueError('The current motion model implementation only supports '
-                         'using a sequence length of three.')
-    if FLAGS.handle_motion and not FLAGS.compute_minimum_loss:
-        raise ValueError('Computing the minimum photometric loss is required when '
-                         'enabling object motion handling.')
-    # if FLAGS.size_constraint_weight > 0 and not FLAGS.handle_motion:
-    #   raise ValueError('To enforce object size constraints, enable motion '
-    #                    'handling.')
-    if FLAGS.imagenet_ckpt and not FLAGS.imagenet_norm:
-        logging.warn('When initializing with an ImageNet-pretrained model, it is '
-                     'recommended to normalize the image inputs accordingly using '
-                     'imagenet_norm.')
-    if FLAGS.compute_minimum_loss and FLAGS.seq_length % 2 != 1:
-        raise ValueError('Compute minimum loss requires using an odd number of '
-                         'images in a sequence.')
-    if FLAGS.architecture != nets.RESNET and FLAGS.imagenet_ckpt:
-        raise ValueError('Can only load weights from pre-trained ImageNet model '
-                         'when using ResNet-architecture.')
-    if FLAGS.compute_minimum_loss and FLAGS.exhaustive_mode:
-        raise ValueError('Exhaustive mode has no effect when compute_minimum_loss '
-                         'is enabled.')
-    if FLAGS.img_width % (2 ** 5) != 0 or FLAGS.img_height % (2 ** 5) != 0:
-        logging.warn('Image size is not divisible by 2^5. For the architecture '
-                     'employed, this could cause artefacts caused by resizing in '
-                     'lower dimensions.')
-    if FLAGS.icp_weight > 0.0:
-        # TODO(casser): Change ICP interface to take matrix instead of vector.
-        raise ValueError('ICP is currently not supported.')
+    # Load training parameters
+    data_dir, \
+    using_saved_images, \
+    pretrained_ckpt, \
+    imagenet_ckpt, \
+    checkpoint_dir, \
+    isaac_app_filename, \
+    cuda_device, \
+    file_extension, \
+    batch_size, \
+    save_ckpt_every, \
+    learning_rate, \
+    beta1, \
+    reconstr_weight, \
+    ssim_weight, \
+    smooth_weight, \
+    icp_weight, \
+    size_constraint_weight, \
+    img_height, \
+    img_width, \
+    seq_length, \
+    architecture, \
+    imagenet_norm, \
+    weight_reg, \
+    exhaustive_mode, \
+    random_scale_crop, \
+    flipping_mode, \
+    train_steps, \
+    summary_freq, \
+    depth_upsampling, \
+    depth_normalization, \
+    compute_minimum_loss, \
+    use_skip, \
+    equal_weighting, \
+    joint_encoder, \
+    handle_motion, \
+    master, \
+    shuffle,\
+    max_ckpts_to_keep = load_parameters()
 
-    if not gfile.Exists(FLAGS.checkpoint_dir):
-        gfile.MakeDirs(FLAGS.checkpoint_dir)
+    # Ensure that parameters aren't breaking current model functionality
+    verify_parameters(handle_motion, joint_encoder, seq_length, compute_minimum_loss, img_height, img_width,
+                      imagenet_ckpt, imagenet_norm, architecture, exhaustive_mode, icp_weight, checkpoint_dir)
 
     # Create Isaac application.
     isaac_app = Application(name="carter_sim", modules=["map",
@@ -217,42 +223,46 @@ def main(_):
     # Register custom Isaac codelets
     isaac_app.register({"differential_base_state": DifferentialBaseState})
 
-    train_model = model.Model(data_dir=FLAGS.data_dir,
-                              file_extension=FLAGS.file_extension,
+    # Set the GPU to run
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = cuda_device
+
+    train_model = model.Model(data_dir=data_dir,
+                              file_extension=file_extension,
                               is_training=True,
-                              learning_rate=FLAGS.learning_rate,
-                              beta1=FLAGS.beta1,
-                              reconstr_weight=FLAGS.reconstr_weight,
-                              smooth_weight=FLAGS.smooth_weight,
-                              ssim_weight=FLAGS.ssim_weight,
-                              icp_weight=FLAGS.icp_weight,
-                              batch_size=FLAGS.batch_size,
-                              img_height=FLAGS.img_height,
-                              img_width=FLAGS.img_width,
-                              seq_length=FLAGS.seq_length,
-                              architecture=FLAGS.architecture,
-                              imagenet_norm=FLAGS.imagenet_norm,
-                              weight_reg=FLAGS.weight_reg,
-                              exhaustive_mode=FLAGS.exhaustive_mode,
-                              random_scale_crop=FLAGS.random_scale_crop,
-                              flipping_mode=FLAGS.flipping_mode,
-                              depth_upsampling=FLAGS.depth_upsampling,
-                              depth_normalization=FLAGS.depth_normalization,
-                              compute_minimum_loss=FLAGS.compute_minimum_loss,
-                              use_skip=FLAGS.use_skip,
-                              joint_encoder=FLAGS.joint_encoder,
-                              shuffle=True,
-                              handle_motion=FLAGS.handle_motion,
-                              equal_weighting=FLAGS.equal_weighting,
-                              size_constraint_weight=FLAGS.size_constraint_weight,
+                              learning_rate=learning_rate,
+                              beta1=beta1,
+                              reconstr_weight=reconstr_weight,
+                              smooth_weight=smooth_weight,
+                              ssim_weight=ssim_weight,
+                              icp_weight=icp_weight,
+                              batch_size=batch_size,
+                              img_height=img_height,
+                              img_width=img_width,
+                              seq_length=seq_length,
+                              architecture=architecture,
+                              imagenet_norm=imagenet_norm,
+                              weight_reg=weight_reg,
+                              exhaustive_mode=exhaustive_mode,
+                              random_scale_crop=random_scale_crop,
+                              flipping_mode=flipping_mode,
+                              depth_upsampling=depth_upsampling,
+                              depth_normalization=depth_normalization,
+                              compute_minimum_loss=compute_minimum_loss,
+                              use_skip=use_skip,
+                              joint_encoder=joint_encoder,
+                              shuffle=shuffle,
+                              handle_motion=handle_motion,
+                              equal_weighting=equal_weighting,
+                              size_constraint_weight=size_constraint_weight,
                               isaac_app=isaac_app)
 
-    train(train_model, FLAGS.pretrained_ckpt, FLAGS.imagenet_ckpt,
-          FLAGS.checkpoint_dir, FLAGS.train_steps, FLAGS.summary_freq, isaac_app)
+    train(train_model, pretrained_ckpt, imagenet_ckpt,
+          checkpoint_dir, train_steps, summary_freq, isaac_app, max_ckpts_to_keep, save_ckpt_every)
 
-save_checkpoint = 20
+
 def train(train_model, pretrained_ckpt, imagenet_ckpt, checkpoint_dir,
-          train_steps, summary_freq, isaac_app):
+          train_steps, summary_freq, isaac_app, max_ckpts_to_keep, save_ckpt_every):
     """Train model."""
 
     vars_to_restore = None
@@ -265,7 +275,7 @@ def train(train_model, pretrained_ckpt, imagenet_ckpt, checkpoint_dir,
     pretrain_restorer = tf.train.Saver(vars_to_restore)
     vars_to_save = util.get_vars_to_save_and_restore()
     vars_to_save[train_model.global_step.op.name] = train_model.global_step
-    saver = tf.train.Saver(vars_to_save, max_to_keep=MAX_TO_KEEP)
+    saver = tf.train.Saver(vars_to_save, max_to_keep=max_ckpts_to_keep)
     sv = tf.train.Supervisor(logdir=checkpoint_dir, save_summaries_secs=0,
                              saver=None)
     config = tf.ConfigProto()
@@ -329,7 +339,7 @@ def train(train_model, pretrained_ckpt, imagenet_ckpt, checkpoint_dir,
                     train_epoch, train_step, steps_per_epoch, this_cycle,
                     time.time() - start_time, results['loss'])
 
-            if step % save_checkpoint == 0:
+            if step % save_ckpt_every == 0:
                 logging.info('[*] Saving checkpoint to %s...', checkpoint_dir)
                 saver.save(sess, os.path.join(checkpoint_dir, 'model'),
                            global_step=global_step)
